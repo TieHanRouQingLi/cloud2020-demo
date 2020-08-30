fde72c596156edbbddfc0b243887fa2b520a80a0
tiehanrouqing <lhjxm11@163.com>
2020-08-30 15:25:01 +0800

    完善服务提供者集群

---
+++ cloud-provider-payment8002/src/main/java/com/lhj/springcloud/dao/PaymentDao.java
@@ -0,0 +1,13 @@
+package com.lhj.springcloud.dao;
+
+import com.lhj.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param("id") Long id);
+}
