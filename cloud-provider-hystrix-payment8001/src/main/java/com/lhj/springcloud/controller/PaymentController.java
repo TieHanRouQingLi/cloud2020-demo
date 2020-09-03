58ccfcea731725bb7cec389243f85ec58028e5af
hongjie.li <hongjie.li@126.com>
2020-09-03 09:55:58 +0800

    整合Hystrix服务熔断代码

--- cloud-provider-hystrix-payment8001/src/main/java/com/lhj/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/lhj/springcloud/controller/PaymentController.java
@@ -47,4 +47,16 @@ public class PaymentController {
     }
 
 
+    /**
+     * 服务熔断
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("*******result:"+result);
+        return result;
+    }
+
 }
